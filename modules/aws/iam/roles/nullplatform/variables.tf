f88156f1841a8c2142a929cc77586b8378f3d260
David Cohan <cohandv@gmail.com>
2025-02-18 16:52:06 -0300

    [aws_modules_v5] iam roles

---
+++ modules/aws/iam/roles/nullplatform/variables.tf
@@ -0,0 +1,27 @@
+variable "scope_manager_assume_role" {
+  type    = string
+  default = "arn:aws:iam::283477532906:role/scope_and_deploy_manager"
+}
+
+variable "telemetry_manager_assume_role" {
+  type    = string
+  default = "arn:aws:iam::283477532906:role/telemetry_manager"
+}
+
+variable "application_manager_assume_role" {
+  type    = string
+  default = "arn:aws:iam::283477532906:role/application_manager"
+}
+
+variable "assets_bucket_arns" {
+  type        = list(string)
+  description = "Assets bucket arn"
+}
+variable "parameters_bucket_arns" {
+  type        = list(string)
+  description = "Parameters bucket arn"
+}
+variable "parameters_encryption_arns" {
+  type        = list(string)
+  description = "Parameters secret arn"
+}
