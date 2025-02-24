b3592114718c1b4765f2fc96d2ee2b2316af43c5
David Cohan <cohandv@gmail.com>
2025-02-24 11:25:48 -0500

    [service_spec] added basic service spec with actions (#10)

---
+++ modules/nullplatform/service/actions_spec.tf
@@ -0,0 +1,12 @@
+resource "nullplatform_action_specification" "basic_actions" {
+  for_each = var.basic_actions
+
+  name                     = each.value.name
+  type                     = each.key
+  service_specification_id = nullplatform_service_specification.service_spec.id
+  retryable                = each.value.retryable
+
+  parameters = jsonencode(each.value.parameters)
+  results    = jsonencode(each.value.results)
+}
+
