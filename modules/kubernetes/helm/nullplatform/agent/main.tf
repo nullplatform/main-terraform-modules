c9b567e975d0ae87a10b00d5fefda1c78238ef72
sebas_correa <sebastian.correa@nullplatform.io>
2025-08-08 18:21:41 -0300

    fix(infra-config): BAC-1707 update resources req/lim

--- modules/kubernetes/helm/nullplatform/agent/main.tf
+++ modules/kubernetes/helm/nullplatform/agent/main.tf
@@ -12,6 +12,8 @@ locals {
     vault_url            = var.vault_url
     agent_memory_request = var.agent_memory_request
     agent_memory_limit   = var.agent_memory_limit
+    agent_cpu_request = var.agent_cpu_request
+    agent_cpu_limit   = var.agent_cpu_limit
   })
 }
 # Helm release
