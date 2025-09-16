c1652370b05f2e707afb686fbfd3dfc55ec8d6a2
Gabriel Eisbruch <gabriel@nullplatform.com>
2025-09-16 16:20:41 -0300

    Add outputs all vars

--- modules/nullplatform/scope-definition/outputs.tf
+++ modules/nullplatform/scope-definition/outputs.tf
@@ -24,3 +24,29 @@ output "action_specification_ids" {
   description = "Map of action specification names to their IDs"
 }
 
+output "nrn" {
+  value       = var.nrn
+  description = "The NRN of the created service specification"
+}
+output "github_repo_url" {
+  value       = var.github_repo_url
+  description = "The GitHub repository URL associated with the service specification"
+}
+output "github_ref" {
+  value       = var.github_ref
+  description = "The GitHub branch associated with the service specification"
+}
+output "github_scope_path" {
+  value       = var.github_scope_path
+  description = "The GitHub path associated with the service specification"
+}
+
+output "scope_name" {
+  value       = var.scope_name
+  description = "The name of the scope definition"
+}
+
+output "scope_description" {
+  value       = var.scope_description
+  description = "The name of the scope definition"
+}
