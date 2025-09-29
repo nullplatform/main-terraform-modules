3b289cd57df01f63fe40bfc5a769ad3e638341f9
David Fernandez <rodavidfernandez@gmail.com>
2025-09-29 15:33:43 -0300

    feat: add prometheus

---
+++ workloads/prometheus/main.tf
@@ -0,0 +1,24 @@
+resource "helm_release" "prometheus" {
+  name             = "prometheus"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "prometheus"
+  namespace        = var.namespace
+  create_namespace = true
+
+  values = [ local.prometheus-values ]
+}
+
+resource "nullplatform_provider_config" "prometheus" {
+  nrn  = var.nrn
+  type = "prometheus"
+  attributes = jsonencode({
+    "server" : {
+      "url" : "http://prometheus-server.${var.namespace}.svc.cluster.local:80"
+    }
+  })
+  dimensions = {}
+
+  lifecycle {
+    ignore_changes = [attributes]
+  }
+}
