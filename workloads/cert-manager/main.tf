aef9c74515e67a3ebbe54f3233a661bef496887d
David Fernandez <rodavidfernandez@gmail.com>
2025-09-30 17:54:25 -0300

    feat: add istios and cert manager config

---
+++ workloads/cert-manager/main.tf
@@ -0,0 +1,26 @@
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = var.cert_manager_namespace
+  create_namespace = true
+  version          = var.cert_manager_version
+
+  set = [{
+    name  = "crds.enabled"
+    value = "true"
+    }
+  ]
+}
+
+
+resource "helm_release" "cert_manager-config" {
+  name             = "cert-manager-config"
+  repository       = "https://nullplatform.github.io/helm-charts"
+  chart            = "nullplatform-cert-manager-config"
+  create_namespace = true
+  version          = var.cert_manager_config_version
+
+  values = [local.helm_values]
+}
+
