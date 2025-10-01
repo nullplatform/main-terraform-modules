5a38b5db00dbb386c0c56d32b442bfdae89e03e5
David Fernandez <rodavidfernandez@gmail.com>
2025-10-01 15:03:18 -0300

    feat:rename resource

--- workloads/cert-manager/main.tf
+++ workloads/cert-manager/main.tf
@@ -20,6 +20,7 @@ resource "helm_release" "cert_manager_config" {
   chart            = "nullplatform-cert-manager-config"
   create_namespace = true
   version          = var.cert_manager_config_version
+  namespace        = var.cert_manager_namespace
 
   values = [local.helm_values]
 }
