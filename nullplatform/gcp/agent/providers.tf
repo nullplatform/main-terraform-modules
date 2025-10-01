3d0db1200b54df1086d78bb595a2ddccff764372
David Fernandez <rodavidfernandez@gmail.com>
2025-10-01 09:41:48 -0300

    feat: remove aws reference

--- nullplatform/gcp/agent/providers.tf
+++ nullplatform/gcp/agent/providers.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "nullplatform/nullplatform"
       version = "~> 0.0.63"
     }
-    /* cambiar por gcp */
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
     helm = {
       source  = "hashicorp/helm"
       version = "~> 3.0"
